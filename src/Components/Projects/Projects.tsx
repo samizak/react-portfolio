e1e5ff0b063403dc8ee971f56aecf5a3091efba2
Sami Zakir Ahmed <54m1z4k@gmail.com>
2022-07-28 19:57:37 +0100

    feat: Moved project links to the right of Project title

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -4,9 +4,11 @@ import opensea_nft_floor_price from "../../Images/opensea-nft-floor-price.png";
 import password_generator from "../../Images/password-generator.png";
 import todo_app from "../../Images/todo-app-showcase.png";
 import portfolio from "../../Images/portfolio.png";
+import url_shortener from "../../Images/url-shortener-showcase.png";
 
 import ExternalLink from "../SVGs/ExternalLink";
 import Github from "../SVGs/Github";
+import Youtube from "../SVGs/Youtube";
 
 export default function Projects() {
   return (
@@ -25,15 +27,35 @@ export default function Projects() {
             </div>
 
             <div className="project-contents">
-              <h3>
-                <a
-                  href="https://sz-nft-portfolio-tracker.herokuapp.com/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  NFT Portfolio Tracker
-                </a>
-              </h3>
+              <div className="project-title">
+                <h3>
+                  <a
+                    href="https://sz-nft-portfolio-tracker.herokuapp.com/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    NFT Portfolio Tracker
+                  </a>
+                </h3>
+                <div className="project-links">
+                  <a
+                    href="https://www.youtube.com/watch?v=7n7F3ATXpR8"
+                    title="Youtube Link"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Youtube />
+                  </a>
+                  <a
+                    href="https://sz-nft-portfolio-tracker.herokuapp.com/"
+                    title="Visit Site"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <ExternalLink />
+                  </a>
+                </div>
+              </div>
               <p>
                 A React application, requested by a friend, that tracks the
                 value of your NFT portfolio and displays the total portfolio
@@ -42,16 +64,15 @@ export default function Projects() {
                 <br></br>
                 <span className="highlight technology-title">Technology</span>
                 <br></br>
-                This website uses NodeJS as backend to handling external APIs,
-                the OpenSea API was used for fetching NFTs, Web3.js was used for
+                This website uses NodeJS as backend to handle external APIs, the
+                OpenSea API was used for fetching NFTs, Web3.js was used for
                 validating Ethereum addresses or Ethereum Name Services (ENS)
                 and the Coingecko API was used for fetching the Ethereum price
                 in different fiat currencies.
                 <br></br>
                 <br></br>
                 <span className="highlight">
-                  This project is being used privately in the wild. Source code
-                  and demos can be shown upon request!
+                  Source code can be shown upon request!
                 </span>
               </p>
               <ul className="project-tech-list underline-tech-list">
@@ -67,17 +88,6 @@ export default function Projects() {
                 <li>Web3.js</li>
                 <li>Heroku</li>
               </ul>
-
-              <div className="project-links">
-                <a
-                  href="https://sz-nft-portfolio-tracker.herokuapp.com/"
-                  title="External Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <ExternalLink />
-                </a>
-              </div>
             </div>
           </div>
 
@@ -92,15 +102,36 @@ export default function Projects() {
             </div>
 
             <div className="project-contents">
-              <h3>
-                <a
-                  href="https://sz-opensea-floor-price.herokuapp.com/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  OpenSea NFT Floor Price Tracker
-                </a>
-              </h3>
+              <div className="project-title">
+                <h3>
+                  <a
+                    href="https://sz-opensea-floor-price.herokuapp.com/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    OpenSea NFT Floor Price Tracker
+                  </a>
+                </h3>
+                <div className="project-links">
+                  <a
+                    href="https://www.youtube.com/watch?v=Gsh5KQ8EaeU"
+                    title="Youtube Link"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Youtube />
+                  </a>
+                  <a
+                    href="https://sz-opensea-floor-price.herokuapp.com/"
+                    title="Visit Site"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <ExternalLink />
+                  </a>
+                </div>
+              </div>
+
               <p>
                 A React application that lets you retrieve the floor price of
                 the queried NFT on OpenSea.
@@ -116,8 +147,7 @@ export default function Projects() {
                 <br></br>
                 <br></br>
                 <span className="highlight">
-                  This project is being used privately in the wild. Source code
-                  and demos can be shown upon request!
+                  Source code can be shown upon request!
                 </span>
               </p>
               <ul className="project-tech-list underline-tech-list">
@@ -133,17 +163,6 @@ export default function Projects() {
                 <li>Axios</li>
                 <li>Heroku</li>
               </ul>
-
-              <div className="project-links">
-                <a
-                  href="https://sz-opensea-floor-price.herokuapp.com/"
-                  title="External Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <ExternalLink />
-                </a>
-              </div>
             </div>
           </div>
 
@@ -155,15 +174,33 @@ export default function Projects() {
             </div>
 
             <div className="project-contents">
-              <h3>
-                <a
-                  href="https://sz-react-password-generator.netlify.app/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  React Portfolio Website
-                </a>
-              </h3>
+              <div className="project-title">
+                <h3>
+                  <a href="" rel="noopener noreferrer" target="_blank">
+                    React Portfolio Website
+                  </a>
+                </h3>
+                <div className="project-links">
+                  <a
+                    href="https://github.com/samizak/react-portfolio"
+                    title="Github Link"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Github />
+                  </a>
+
+                  <a
+                    href=""
+                    title="Visit Site"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <ExternalLink />
+                  </a>
+                </div>
+              </div>
+
               <p>
                 My current Portfolio website created in React.
                 <br></br>
@@ -181,80 +218,68 @@ export default function Projects() {
                 <li>CSS</li>
                 <li>Netlify</li>
               </ul>
-
-              <div className="project-links">
-                <a
-                  href="https://github.com/samizak/react-portfolio"
-                  title="Github Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Github />
-                </a>
-
-                <a
-                  href="https://sz-react-password-generator.netlify.app/"
-                  title="External Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <ExternalLink />
-                </a>
-              </div>
             </div>
           </div>
 
           <div className="project-container">
             <div className="project-image-container">
               <div className="project-image-container-inner">
-                <img src={""} alt="URL Shortener" />
+                <img src={url_shortener} alt="URL Shortener" />
               </div>
             </div>
 
             <div className="project-contents">
-              <h3>
-                <a
-                  href="https://sz-short.herokuapp.com/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  URL Shortener Service
-                </a>
-              </h3>
+              <div className="project-title">
+                <h3>
+                  <a
+                    href="https://sz-short.herokuapp.com/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    URL Shortener Service
+                  </a>
+                </h3>
+                <div className="project-links">
+                  <a
+                    href="https://github.com/samizak/react-url-shortener"
+                    title="Github Link"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Github />
+                  </a>
+
+                  <a
+                    href="https://sz-short.herokuapp.com/"
+                    title="Visit Site"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <ExternalLink />
+                  </a>
+                </div>
+              </div>
               <p>
                 A URL shortener service designed in React that uses hooks to
                 shrink lengthy URLs.
+                <br></br>
+                <br></br>
+                <span className="highlight technology-title">Technology</span>
+                <br></br>
+                The website was created using React with the MERN Stack and
+                styled using Bootstrap 5 and CSS.
               </p>
               <ul className="project-tech-list underline-tech-list">
                 <li>React</li>
                 <li>Typescript</li>
                 <li>HTML</li>
                 <li>CSS</li>
+                <li>Bootstrap 5</li>
                 <li>NodeJS</li>
                 <li>Express</li>
                 <li>Mongoose</li>
                 <li>Heroku</li>
               </ul>
-
-              <div className="project-links">
-                <a
-                  href="https://github.com/samizak/react-url-shortener"
-                  title="Github Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Github />
-                </a>
-
-                <a
-                  href="https://sz-short.herokuapp.com/"
-                  title="External Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <ExternalLink />
-                </a>
-              </div>
             </div>
           </div>
 
@@ -266,15 +291,37 @@ export default function Projects() {
             </div>
 
             <div className="project-contents">
-              <h3>
-                <a
-                  href="https://sz-react-password-generator.netlify.app/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Random Password Generator
-                </a>
-              </h3>
+              <div className="project-title">
+                <h3>
+                  <a
+                    href="https://sz-opensea-floor-price.herokuapp.com/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Random Password Generator
+                  </a>
+                </h3>
+                <div className="project-links">
+                  <a
+                    href="https://github.com/samizak/react-password-generator"
+                    title="Github Link"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Github />
+                  </a>
+
+                  <a
+                    href="https://sz-react-password-generator.netlify.app/"
+                    title="Visit Site"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <ExternalLink />
+                  </a>
+                </div>
+              </div>
+
               <p>
                 A password generator app that uses React with hooks to generate
                 secure, random passwords.
@@ -286,26 +333,6 @@ export default function Projects() {
                 <li>CSS</li>
                 <li>Netlify</li>
               </ul>
-
-              <div className="project-links">
-                <a
-                  href="https://github.com/samizak/react-password-generator"
-                  title="Github Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Github />
-                </a>
-
-                <a
-                  href="https://sz-react-password-generator.netlify.app/"
-                  title="External Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <ExternalLink />
-                </a>
-              </div>
             </div>
           </div>
 
@@ -317,15 +344,37 @@ export default function Projects() {
             </div>
 
             <div className="project-contents">
-              <h3>
-                <a
-                  href="https://sz-react-todo-app.netlify.app/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  To Do App
-                </a>
-              </h3>
+              <div className="project-title">
+                <h3>
+                  <a
+                    href="https://sz-react-todo-app.netlify.app/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    To Do App
+                  </a>
+                </h3>
+                <div className="project-links">
+                  <a
+                    href="https://github.com/samizak/React-Todo-App"
+                    title="Github Link"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Github />
+                  </a>
+
+                  <a
+                    href="https://sz-react-todo-app.netlify.app/"
+                    title="Visit Site"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <ExternalLink />
+                  </a>
+                </div>
+              </div>
+
               <p>
                 A To Do App designed for staying organized with simplicity in
                 mind. The website was created with React using hooks.
@@ -337,26 +386,6 @@ export default function Projects() {
                 <li>CSS</li>
                 <li>Netlify</li>
               </ul>
-
-              <div className="project-links">
-                <a
-                  href="https://github.com/samizak/React-Todo-App"
-                  title="Github Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Github />
-                </a>
-
-                <a
-                  href="https://sz-react-todo-app.netlify.app/"
-                  title="External Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <ExternalLink />
-                </a>
-              </div>
             </div>
           </div>
         </div>
